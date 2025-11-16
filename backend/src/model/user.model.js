ba8f6f90879f2ef5674263860963ae62eeaa284a
Vinamra Sahu <vinamras84@gmail.com>
2025-11-16 21:40:26 +0530

    register change

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     fullname: {
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true }
+        firstname: { type: String, },
+        lastname: { type: String,  }
     },
     password: { type: String, required: true }
 }, { timestamps: true });
